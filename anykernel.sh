40cdfa97fc98e7b102160fd0da8d3ae9ad51b67e
Bachpooh <76820483+bachnxuan@users.noreply.github.com>
2025-04-03 22:26:13 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by TheWildJames or Morgan Weedman
+kernel.string=ESK Kernel by bachnxuan | Telegram : @reverdd | Channel: @xaga_kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=0
